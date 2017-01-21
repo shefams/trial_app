89e6c2c529e3466348bfdc91709e445f13cafb92
Sheriff Adams <sheriffadamz@gmail.com>
2017-01-21 03:52:16 +0200

    add gems

---
+++ trial_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrialApp::Application.config.secret_key_base = 'a6e3734fc87ac0ae3ea2cb5e3d0eca4ae00a78b9160ff86d3f9f37e2bc796af8ec95c3507a954d3b796b052771cbbab5778cd37fe0ea997312e7389709ff2cbe'
---
+++ trial_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrialApp::Application.config.session_store :cookie_store, key: '_trial_app_session'
